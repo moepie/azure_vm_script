4556c80af81643f0788bdee609cb0b68389059ab
moepie <529291810@qq.com>
2021-09-09 15:37:40 +0800

    update git link

--- lb_init.sh
+++ lb_init.sh
@@ -2,6 +2,6 @@
 
 yum install -y git gcc python3 python3-devel
 pip3 install flask
-git clone https://github.com/collinsctk/Azure_K8S_DevOps.git
+git clone https://git@github.com:moepie/Azure_K8S_DevOps.git
 cd Azure_K8S_DevOps/
 python3 app.py &
